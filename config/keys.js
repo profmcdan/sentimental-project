157e22bd66b2c737a8fedff52932200b74b7a95e
Daniel Ale <denielale9291@gmail.com>
2018-09-13 10:35:27 +0100

    For Heroku

---
+++ config/keys.js
@@ -0,0 +1,30 @@
+// add this file to .gitignore
+module.exports = {
+  google: {
+    clientID:
+      "1064799382955-kakfl7hdsfrut78nsu3vnvigkj93554p.apps.googleusercontent.com",
+    clientSecret: "YYEXptv95_iy5INH6aYYs0lJ"
+  },
+  github: {
+    clientID: "5b30b140bff21f5803b7",
+    clientSecret: "4f7a0365160ece17ed9875a2313b7e657d99afea"
+  },
+  mongodb: {
+    dbURI: "mongodb://mcdan:user4321@ds119052.mlab.com:19052/nn-auth"
+  },
+  session: {
+    cookieKey: "icanhehtyurjnghnk575768nnfhndvj4iunfurnnbg"
+  },
+  twitter: {
+    consumer_key: "Z4rvceDMzZnv696OwooysJLVa",
+    consumer_secret: "Ovv7QVemhQS1uw6C5Ogt8T2169albDmynE2mvKdxIuXlWy1BGZ",
+    token: "tldh7TYEuC5jEb632yu3NGzdJZggTvRLAPFkWK2",
+    token_secret: "yY61tpWV8KgExBJiFdME71P5XW6HneWCSfAwhKxS46gtQ"
+  },
+  hp: "e13f798c-caf0-480e-97ae-619f115dc55b",
+  watson: {
+    username: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    password: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  },
+  googlePlaces: "AIzaSyAmQ2SEVMrmnTnYGtD2JR6InW9serVBZl4"
+};
