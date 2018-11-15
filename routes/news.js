af975ddd82f541c532e4ebbff813eee6ec7941a5
Daniel Ale <danielale9291@gmail.com>
2018-11-15 11:56:23 +0100

    Added News Feed API

--- routes/news.js
+++ routes/news.js
@@ -2,31 +2,45 @@ const router = require("express").Router();
 const keys = require("../config/keys");
 const { google } = require("googleapis");
 const customSearch = google.customsearch("v1");
+const newsAPIKey = require("../config/keys").newApiKey;
 
 async function runSample(options) {
-  console.log(options);
-  const res = await customSearch.cse.list({
-    cx: options.cx,
-    q: options.q,
-    auth: options.apiKey
-  });
-  console.log(res.data);
-  return res.data;
+	console.log(options);
+	const res = await customSearch.cse.list({
+		cx: options.cx,
+		q: options.q,
+		auth: options.apiKey
+	});
+	console.log(res.data);
+	return res.data;
 }
 
+router.get("/custom/:keyword", (req, res) => {
+	customSearch.cse
+		.list({
+			cx: keys.googleCustomSearch.engine,
+			q: req.params.keyword,
+			auth: keys.googleCustomSearch.apiKey
+		})
+		.then((results) => {
+			return res.json(results.data.items);
+		})
+		.catch((err) => {
+			return res.status(400).json(err);
+		});
+});
+
 router.get("/:keyword", (req, res) => {
-  customSearch.cse
-    .list({
-      cx: keys.googleCustomSearch.engine,
-      q: req.params.keyword,
-      auth: keys.googleCustomSearch.apiKey
-    })
-    .then(results => {
-      return res.json(results.data.items);
-    })
-    .catch(err => {
-      return res.status(400).json(err);
-    });
+	var url = `https://newsapi.org/v2/everything?q=${req.params
+		.keyword}&from=2018-11-15&sortBy=popularity&apiKey=${newsAPIKey}`;
+	var request = new Request(url);
+	fetch(request)
+		.then(function(response) {
+			return res.send(response.json());
+		})
+		.catch((err) => {
+			return res.status(400).json(err);
+		});
 });
 
 module.exports = router;
